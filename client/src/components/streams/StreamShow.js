5150cfedf529d394fbc4e09c528c381bc2251ad6
EC2 Default User <ec2-user@ip-172-31-37-125.us-west-2.compute.internal>
2019-01-12 00:46:19 +0000

    everything

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
+import flv from 'flv.js';
 
 class StreamShow extends React.Component {
-  componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
   }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: 'flv',
+      //      url: `https://dd953a862aee4260bbeebe5876ae73cb.vfs.cloud9.us-west-2.amazonaws.com:8082/live/${id}.flv`
+      // url: 'https://www.mediacollege.com/video-gallery/testclips/20051210-w50s.flv'
+      url: 'https://dd953a862aee4260bbeebe5876ae73cb.vfs.cloud9.us-west-2.amazonaws.com/converted.flv'
+    })
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
@@ -15,6 +48,7 @@ class StreamShow extends React.Component {
 
     return (
       <div>
+        <video ref={this.videoRef} style={{width: '100%'}} controls/>
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
